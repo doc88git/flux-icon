fda72b8c60c38df29b330841a6b44fe5619f071c
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2020-02-18 14:04:13 -0300

    v1 - require file icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fluxIcons = (name, size = 16) => {
   try {
-    return `./assets/${size}px/${name}-${size}px.svg`
+    return require(`./assets/${size}px/${name}-${size}px.svg`)
   } catch (e) {
     console.error('File not found')
   }
