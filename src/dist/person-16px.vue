599127504a5deb7d8dc7481f8c099b1bfdfd2a8a
Gabriel Gelado <gbgelado@gmail.com>
2020-03-20 12:09:13 -0300

    Fix build vue components (#3)
    * Adição de novos ícones
    * Novos icones de usuário
    * update version 1.1.0
    * Build Vue Icons
    
    Co-authored-by: Rud Passos <rudpassos@gmail.com>
    Co-authored-by: Gabriel Gelado <gabriel.gelado@doc88.com.br>

---
+++ src/dist/person-16px.vue
@@ -0,0 +1,5 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 16"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M4,4A2,2,0,1,1,6,2,2,2,0,0,1,4,4ZM4,1A1,1,0,1,0,5,2,1,1,0,0,0,4,1ZM3,15.5v-7a.5.5,0,0,0-1,0v7a.5.5,0,0,0,1,0Zm3,0v-7a.5.5,0,0,0-1,0v7a.5.5,0,0,0,1,0Zm2-4v-5A1.5,1.5,0,0,0,6.5,5h-5A1.5,1.5,0,0,0,0,6.5v5a.5.5,0,0,0,.5.5.5.5,0,0,0,.5-.5v-5A.5.5,0,0,1,1.5,6h5a.5.5,0,0,1,.5.5v5a.5.5,0,0,0,1,0Z"/></g></g></svg></template>
+<script> 
+ export default { name: "person" }
+</script>
---
+++ src/dist/water-24px.vue
@@ -0,0 +1,5 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.38 23.98"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M9.69,24A9.66,9.66,0,0,1,2.35,8L8.93.33a1,1,0,0,1,1.52,0L17,8A9.66,9.66,0,0,1,9.69,24Zm0-21.46L3.86,9.32A7.67,7.67,0,0,0,9.69,22,7.67,7.67,0,0,0,15.52,9.32Zm6,11.46a1,1,0,0,0-2,0,4,4,0,0,1-4,4,1,1,0,0,0,0,2A6,6,0,0,0,15.69,14Z"/></g></g></svg></template>
+<script> 
+ export default { name: "water" }
+</script>
