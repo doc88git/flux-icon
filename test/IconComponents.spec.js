5dafd1150e816726c9e2ca604a74116c1ba79d13
Gabriel Gelado <gbgelado@gmail.com>
2020-03-30 15:14:26 -0300

    add: test config

---
+++ test/IconComponents.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+
+describe('Icon Components tests', () => {
+  let WRAPPER
+
+  beforeEach(() => (WRAPPER = shallowMount(FPagination, { propsData: WRAPPER_PROPS })))
+})
