6c60960e8ace4746c544daf8fe111452c84c0d3d
Gabriel Gelado <gbgelado@gmail.com>
2020-06-09 18:48:01 -0300

    [Rudawesome] New Icons (#14)
    * correções
    
    * adição de icone dashboard
    
    * atualização de ícone
    
    Laptop
    
    * correção nome
    
    * adição de novo ícone
    
    archive 16px e 24px
    
    * novos ícones:
    
    • thumbs-down
    • thumbs-up
    
    * Novos ícones
    
    Mover Widget
    Mover Vertical
    Redimensionar Diagonal
    Redimensionar Vertical
    Redimensionar Horizontal
    Elipses
    Elipses Vertical
    Arquivo de Imagem
    Gráfico de área
    Gráfico barra horizontal
    Gráfico Donut
    
    * new icons
    
    Co-authored-by: Rud Passos <rudpassos@gmail.com>

---
+++ src/dist/chart-donut-24px.vue
@@ -0,0 +1,5 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0ZM22,12a9.94,9.94,0,0,1-2.26,6.32l-2.85-2.85A6,6,0,0,0,13,6.09v-4A10,10,0,0,1,22,12ZM12,8a4,4,0,1,1-4,4A4,4,0,0,1,12,8ZM11,2.05v4A6,6,0,0,0,6.09,11h-4A10,10,0,0,1,11,2.05ZM2.05,13h4a6,6,0,0,0,9.38,3.89l2.85,2.85A10,10,0,0,1,2.05,13Z"/></g></g></svg></template>
+<script> 
+ export default { name: "chart-donut" }
+</script>
